7c0d0b2792c8b5511a2c7295232103e5092accf4
udu-beni <ehimareehimen1@gmail.com>
2022-05-06 19:34:57 +0100

    new updates

--- HeartPredictor/settings.py
+++ HeartPredictor/settings.py
@@ -82,18 +82,18 @@ WSGI_APPLICATION = 'HeartPredictor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'anthony1',
-        'USER': 'postgres',
-        'PASSWORD': '@Ehimare212',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': '',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
-#db_from_env =dj_database_url.config()
+db_from_env =dj_database_url.config()
 
-#DATABASES['default']=db_from_env
+DATABASES['default']=db_from_env
 
 
 # Password validation
